b1208739657f9044d7142f94c800b8cc5069a69b
lundsby <[morten@playpenlabs.com]>
2012-12-02 19:26:07 -0800

    Add static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TaskList::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/tasks"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
