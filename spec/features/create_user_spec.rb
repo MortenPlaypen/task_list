bf4b586c90515e25b6944d79b46af85f1c761e6e
lundsby <[morten@playpenlabs.com]>
2012-12-16 14:54:49 -0800

    Adding green tests

---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Sign up path" do
+
+  
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    #it { should have_selector('h1', text: 'Sign up') }
+    
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        #save_and_open_page
+        expect { click_on submit}.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_on submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+end
---
+++ spec/features/user_pages_spec.rb
@@ -0,0 +1,14 @@
+# require 'spec_helper'
+
+# describe "User pages" do
+
+#   subject { page }
+
+#   describe "profile page" do
+#     let(:user) { FactoryGirl.create(:user) }
+#     before { visit user_path(user) }
+# 	#save_and_open_page
+#     it { should have_selector('h1',    text: user.name) }
+#     it { should have_selector('title', text: user.name) }
+#   end
+# end
